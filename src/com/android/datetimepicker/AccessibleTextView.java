ce86b5f0053a409c7a236d266511bb510c455edf
Ismael Reyes <ismael.kix2902@gmail.com>
2013-08-31 21:53:18 +0200

    AccessibleViews made compatible with Android 2.1+.

--- src/com/android/datetimepicker/AccessibleTextView.java
+++ src/com/android/datetimepicker/AccessibleTextView.java
@@ -17,9 +17,12 @@
 package com.android.datetimepicker;
 
 import android.content.Context;
+import android.support.v4.view.AccessibilityDelegateCompat;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.view.accessibility.AccessibilityNodeInfoCompat;
 import android.util.AttributeSet;
+import android.view.View;
 import android.view.accessibility.AccessibilityEvent;
-import android.view.accessibility.AccessibilityNodeInfo;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -28,19 +31,40 @@ import android.widget.TextView;
  */
 public class AccessibleTextView extends TextView {
 
-    public AccessibleTextView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-    }
+	public AccessibleTextView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		installAccessibilityDelegate();
+	}
 
-    @Override
-    public void onInitializeAccessibilityEvent(AccessibilityEvent event) {
-        super.onInitializeAccessibilityEvent(event);
-        event.setClassName(Button.class.getName());
-    }
+	private void installAccessibilityDelegate() {
+		// The accessibility delegate enables customizing accessibility behavior
+		// via composition as opposed as inheritance. The main benefit is that
+		// one can write a backwards compatible application by setting the delegate
+		// only if the API level is high enough i.e. the delegate is part of the APIs.
+		// The easiest way to achieve that is by using the support library which
+		// takes the burden of checking API version and knowing which API version
+		// introduced the delegate off the developer.
+		ViewCompat.setAccessibilityDelegate(this, new AccessibilityDelegateCompat() {
 
-    @Override
-    public void onInitializeAccessibilityNodeInfo(AccessibilityNodeInfo info) {
-        super.onInitializeAccessibilityNodeInfo(info);
-        info.setClassName(Button.class.getName());
-    }
+			@Override
+			public void onInitializeAccessibilityEvent(View host, AccessibilityEvent event) {
+				super.onInitializeAccessibilityEvent(host, event);
+				// Note that View.onInitializeAccessibilityNodeInfo was introduced in
+				// ICS and we would like to tweak a bit the text that is reported to
+				// accessibility services via the AccessibilityNodeInfo.
+				event.getText().add(Button.class.getName());
+			}
+
+			@Override
+			public void onInitializeAccessibilityNodeInfo(View host,
+					AccessibilityNodeInfoCompat info) {
+				super.onInitializeAccessibilityNodeInfo(host, info);
+				// Note that View.onInitializeAccessibilityNodeInfo was introduced in
+				// ICS and we would like to tweak a bit the text that is reported to
+				// accessibility services via the AccessibilityNodeInfo.
+				info.setText(Button.class.getName());
+			}
+		});
+	}
+
 }
